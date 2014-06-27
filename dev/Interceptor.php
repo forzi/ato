1771b23e3fbb6dd44b7453d54c69e7fec4f89b10
Penkov Alexey <forziforzi@gmail.com>
2014-06-27 16:46:05 +0300

    Working with exceptions

--- dev/Interceptor.php
+++ dev/Interceptor.php
@@ -1,9 +1,25 @@
 <?
 namespace stradivari\stradivari_default {
-    abstract class Interceptor {
+use \stradivari\core\App;
+    abstract class Interceptor extends \stradivari\core\AbstractController {
         public static function catchException(\Exception $exception) {
-            echo '<pre>';
-            throw $exception;
+            if ( isset(App::$pool['input']['argv']) ) {
+                throw $exception;
+            }
+            if ( $exception instanceof \stradivari\interfaces\WebException ) {
+                $error['code'] = $exception->getCode();
+                $error['msg'] = $exception->getMessage();
+                foreach ( $exception->headers() as $header ) {
+                    header($header);
+                }
+            } else {
+                $error['code'] = 500;
+                $error['msg'] = 'Internal Server Error';
+            }
+            if ( $error['code'] ) {
+                header("HTTP/1.1 {$error['code']} {$error['msg']}");
+                echo self::loadView('error', $error);
+            }
         }
         public static function catchError($errno, $errstr, $errfile = '', $errline = 0, $errcontext = array()) {
             d($errno, 0);
@@ -14,7 +30,7 @@ namespace stradivari\stradivari_default {
             return false;
         }
         public static function microtimeEvaluator() {
-            $oldTime = \stradivari\core\App::$pool['tools']['startMicrotime'];
+            $oldTime = App::$pool['tools']['startMicrotime'];
             if ( $oldTime === null ) {
                 var_dump('not set');
                 return false;
--- prod/controller/Main.php
+++ prod/controller/Main.php
@@ -2,7 +2,7 @@
 namespace stradivari\stradivari_default\controller {
     abstract class Main extends \stradivari\core\AbstractController {
         protected static function main__get() {
-            echo 'Antonius Stradivarius Cremonensis Faciebat Anno 2013';
+            echo self::loadView('main');
         }
     }
 }
