020646c273e90c58409fe97abb1dde5802c27f0f
Penkov Alexey <forzi@mail333.com>
2014-11-11 16:56:36 +0200

    Interceptor refactoring

---
+++ prod/AbstractInterceptor.php
@@ -0,0 +1,44 @@
+<?
+namespace stradivari\stradivari_default {
+use \stradivari\core\App;
+    abstract class AbstractInterceptor extends \stradivari\core\AbstractController {
+        public static $exceptionLineCount = 10;
+		
+        public static function catchException(\Exception $exception) {
+            if ( isset(App::$pool['input']['argv']) ) {
+                throw $exception;
+            }
+            if ( $exception instanceof \stradivari\interfaces\WebException ) {
+                $error['code'] = $exception->getCode();
+                $error['msg'] = $exception->getMessage();
+                foreach ( $exception->headers() as $header ) {
+                    header($header);
+                }
+            } else {
+                $error['code'] = 500;
+                $error['msg'] = 'Internal Server Error';
+            }
+            if ( $error['code'] && $error['code'] != 200 ) {
+                header("HTTP/1.1 {$error['code']} {$error['msg']}");
+                echo static::loadView($error, $exception);
+            }
+        }
+        public static function catchError($errno, $errstr, $errfile = '', $errline = 0, $errcontext = array()) {			
+			$e = new exception\ErrorException($errstr, $errno, 0, $errfile, $errline);
+			if ( $e->isFatal ) {
+				self::catchException($e);
+				return false;
+			}
+			throw $e;
+		}
+        public static function microtimeEvaluator() {
+            $oldTime = App::$pool['tools']['startMicrotime'];
+            if ( $oldTime === null ) {
+                var_dump('not set');
+                return false;
+            }
+            $newTime = microtime(true);
+            var_dump( number_format($newTime - $oldTime, 4) );
+        }
+    }
+}
--- prod/Interceptor.php
+++ prod/Interceptor.php
@@ -1,42 +1,9 @@
 <?
 namespace stradivari\stradivari_default {
 use \stradivari\core\App;
-    abstract class Interceptor extends \stradivari\core\AbstractController {
-        public static function catchException(\Exception $exception) {
-            if ( isset(App::$pool['input']['argv']) ) {
-                throw $exception;
-            }
-            if ( $exception instanceof \stradivari\interfaces\WebException ) {
-                $error['code'] = $exception->getCode();
-                $error['msg'] = $exception->getMessage();
-                foreach ( $exception->headers() as $header ) {
-                    header($header);
-                }
-            } else {
-                $error['code'] = 500;
-                $error['msg'] = 'Internal Server Error';
-            }
-            if ( $error['code'] && $error['code'] != 200 ) {
-                header("HTTP/1.1 {$error['code']} {$error['msg']}");
-                echo self::loadView('error', $error);
-            }
-        }
-        public static function catchError($errno, $errstr, $errfile = '', $errline = 0, $errcontext = array()) {			
-			$e = new exception\ErrorException($errstr, $errno, 0, $errfile, $errline);
-			if ( $e->isFatal ) {
-				self::catchException($e);
-				return false;
-			}
-			throw $e;
+    abstract class Interceptor extends AbstractInterceptor {
+		public static function loadView($error, $exception) {
+			return parent::loadView('exception', array('exception' => $exception, 'lineCount' => self::$exceptionLineCount));
 		}
-        public static function microtimeEvaluator() {
-            $oldTime = App::$pool['tools']['startMicrotime'];
-            if ( $oldTime === null ) {
-                var_dump('not set');
-                return false;
-            }
-            $newTime = microtime(true);
-            var_dump( number_format($newTime - $oldTime, 4) );
-        }
     }
 }
