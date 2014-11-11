004ca6d56fcb4931ab7ba5132b32df8a4fe88be1
Alexey Penkov <forzi@mail333.com>
2014-11-11 22:42:57 +0200

    change dev and prod

--- dev/Interceptor.php
+++ dev/Interceptor.php
@@ -3,7 +3,7 @@ namespace stradivari\stradivari_default {
 use \stradivari\core\App;
     abstract class Interceptor extends AbstractInterceptor {
 		public static function loadView($error, $exception) {
-			return parent::loadView('error', $error);
+			return parent::loadView('exception', array('exception' => $exception, 'lineCount' => self::$exceptionLineCount));
 		}
     }
 }
